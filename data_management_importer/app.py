b9c950015c217cc0901b05160f64bb0ceb172d8c
Zach Wright <zwright@umich.edu>
2022-11-04 10:33:16 -0400

    KPMP-3955: Add tests

--- data_management_importer/app.py
+++ data_management_importer/app.py
@@ -5,11 +5,10 @@ from services.dlu import dlu_package_dict_to_tuple, dlu_file_dict_to_tuple
 
 app = Flask(__name__)
 CORS(app)
-data_management = DataManagement()
-
 
 @app.route("/v1/dlu/package", methods=["POST"])
 def add_dlu_package():
+    data_management = DataManagement()
     content = request.json
     content_tuple = dlu_package_dict_to_tuple(content)
     data_management.insert_dlu_package(content_tuple)
@@ -18,12 +17,14 @@ def add_dlu_package():
 
 @app.route("/v1/dlu/package/<package_id>", methods=["POST"])
 def update_dlu_package(package_id):
+    data_management = DataManagement()
     content = request.json
     data_management.update_dlu_package(package_id, content)
     return package_id
 
 @app.route("/v1/dlu/file", methods=["POST"])
 def add_dlu_file():
+    data_management = DataManagement()
     content = request.json
     content_tuple = dlu_file_dict_to_tuple(content)
     data_management.insert_dlu_file(content_tuple)
--- data_management_importer/tests/test_app.py
+++ data_management_importer/tests/test_app.py
@@ -2,23 +2,61 @@ import unittest
 from unittest import mock
 from app import app
 
+
 @mock.patch('lib.mysql_connection.MYSQLConnection')
 @mock.patch('mysql.connector.connect')
-@mock.patch('services.data_management.DataManagement')
 class TestApp(unittest.TestCase):
 
-
-    #NOTE: These don't work
-    def test_add_dlu_file(self, mock_datamanagement_constructor, mock_mysqlconnection_constructor, mock_connect):
-        mock_data_management = mock_datamanagement_constructor.return_value
-        mock_db = mock_mysqlconnection_constructor.return_value
-        mock_data_management.db = mock_db
-        app.data_manager = mock_data_management
-        app.data_manager.db.get_db_connection.return_value = ""
-        mock_connect.return_value = ""
+    def test_add_dlu_file(self, mock_connect, mock_mysqlconnection):
         with app.test_client() as client:
             rv = client.post('/v1/dlu/file', json={
-                'email': 'flask@example.com', 'password': 'secret'}
-                                    )
-            json_data = rv.get_json()
-            assert json_data == "blah"
+                'dluFileName': 'file_name', 'dluPackageId': 'package_id',
+                'dluFileId': "file_id", 'dluFileSize': 1235, 'dluMd5Checksum': 'checksum'}
+                             )
+            assert rv.data.decode() == "file_id"
+
+    def test_add_dlu_package(self, mock_connect, mock_mysqlconnection):
+        with app.test_client() as client:
+            rv = client.post('/v1/dlu/package', json={"dluPackageId": "packageid",
+                                                      "dluCreated": 1667572308,
+                                                      "dluSubmitter": "submitter",
+                                                      "dluTis": "tis",
+                                                      "dluPackageType": "pt",
+                                                      "dluSubjectId": "subject",
+                                                      "dluError": 0,
+                                                      "dluLfu": 1,
+                                                      "knownSpecimen": "speciment",
+                                                      "redcapId": "redcap",
+                                                      "userPackageReady": 1,
+                                                      "dvcValidationComplete": 1,
+                                                      "packageValidated": 1,
+                                                      "readyToPromoteDlu": 0,
+                                                      "promotionDluSucceeded": 1,
+                                                      "removedFromGlobus": 1,
+                                                      "promotionStatus": "promotion",
+                                                      "notes": "notes"}
+                             )
+            assert rv.data.decode() == "packageid"
+
+    def test_update_dlu_package(self, mock_connect, mock_mysqlconnection):
+        with app.test_client() as client:
+            rv = client.post('/v1/dlu/package/packageid', json={"dluPackageId": "packageid",
+                                                            "dluCreated": "created",
+                                                            "dluSubmitter": "submitter",
+                                                            "dluTis": "tis",
+                                                            "dluPackageType": "pt",
+                                                            "dluSubjectId": "subject",
+                                                            "dluError": 0,
+                                                            "dluLfu": 1,
+                                                            "knownSpecimen": "speciment",
+                                                            "redcapId": "redcap",
+                                                            "userPackageReady": 1,
+                                                            "dvcValidationComplete": 1,
+                                                            "packageValidated": 1,
+                                                            "readyToPromoteDlu": 0,
+                                                            "promotionDluSucceeded": 1,
+                                                            "removedFromGlobus": 1,
+                                                            "promotionStatus": "promotion",
+                                                            "notes": "notes"}
+                             )
+            assert rv.data.decode() == "packageid"
