42b23f788b88cb0f3b96d6aaa8e37f85fdef4b3b
Becky Reamy <rlreamy@umich.edu>
2023-05-16 11:35:50 -0400

    KPMP-4407: reverting the threading stuff, just adding the bug fix

--- data_management/app.py
+++ data_management/app.py
@@ -2,8 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from services.data_management import DataManagement
 from services.dlu_utils import dlu_package_dict_to_tuple, dlu_file_dict_to_tuple
-from flask import Response
-from services.dlu_file_move_threading import DLUFileMoveThreaded
+import Response
 
 app = Flask(__name__)
 CORS(app)
@@ -42,8 +41,5 @@ def add_dlu_file():
 def move_dlu_file(package_id):
     data_management = DataManagement()
     data_management.reconnect()
-    # # response = data_management.move_globus_files_to_dlu(package_id)
-    # return Response("Request submitted", status=201, mimetype='application/json')
-    thread = DLUFileMoveThreaded()
-    thread.start()
-    return {"success": True, "message": "Request submitted", "file_list": []}
+    response = data_management.move_globus_files_to_dlu(package_id)
+    return response
--- data_management/services/dlu_file_move_threading.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-class DLUFileMoveThreaded(threading.Thread):
-    def __init__(self):
-        super(DLUFileMoveThreaded, self).__init__()
-
-    def run(self):
-        # run some code here
-        print('Threaded task has been completed')
