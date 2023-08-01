38501e6604a3602292689c0a37f839ef438d542a
Dert1129 <nathancreger1111@gmail.com>
2023-08-01 14:16:21 -0400

    move comma

--- data_management/watcher/dlu_package_inventory.py
+++ data_management/watcher/dlu_package_inventory.py
@@ -27,5 +27,5 @@ class DLUPackageInventory:
     def set_dlu_file_waiting(self, status, package_id):
         return self.db.insert_data(
             "UPDATE data_management.dlu_package_inventory WHERE ready_to_move_from_globus = %s AND dlu_package_id = %s AND globus_dlu_status = waiting",
-            (status, package_id),
+            (status, package_id,)
         )
