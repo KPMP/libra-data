cccba92cd1ef76b977aa2e69df7a1a15d8828be1
Dert1129 <nathancreger1111@gmail.com>
2023-08-01 09:53:43 -0400

    add parentheses

--- data_management/watcher/watch_files.py
+++ data_management/watcher/watch_files.py
@@ -5,7 +5,7 @@ logger = logging.getLogger("services-dlu_package_watcher")
 logger.setLevel(logging.INFO)
      
 def watch_for_files():
-    db = DLUPackageInventory
+    db = DLUPackageInventory()
     files = db.get_dlu_file(db, "yes")
     if len(files) == 0:
         logger.info(
