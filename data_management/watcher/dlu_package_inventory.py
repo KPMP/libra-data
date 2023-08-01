bbb221b498b41f493af2193e2eb25a277dc3ff22
Dert1129 <nathancreger1111@gmail.com>
2023-08-01 09:09:30 -0400

    remove data_management

--- data_management/watcher/dlu_package_inventory.py
+++ data_management/watcher/dlu_package_inventory.py
@@ -1,5 +1,5 @@
 import os
-from data_management.lib.mysql_connection import MYSQLConnection
+from lib.mysql_connection import MYSQLConnection
 import logging
 from dotenv import load_dotenv
 load_dotenv()
