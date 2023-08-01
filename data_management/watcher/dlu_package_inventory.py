7d8ea5d2988a86f7de680b3cda6c0e4e6a2dc08e
Dert1129 <nathancreger1111@gmail.com>
2023-08-01 11:06:45 -0400

    fix query

--- data_management/watcher/dlu_package_inventory.py
+++ data_management/watcher/dlu_package_inventory.py
@@ -20,7 +20,7 @@ class DLUPackageInventory:
         
     def get_dlu_file(self, status):
         return self.db.get_data(
-            "SELECT * FROM data_management.dlu_package_inventory WHERE ready_to_move_from_globus = %s",
+            "SELECT * FROM data_management.dlu_package_inventory WHERE ready_to_move_from_globus = %s AND globus_dlu_status IS NULL",
             (status),
         )
     
