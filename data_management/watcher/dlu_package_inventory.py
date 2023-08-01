ff1b1c2860e9857077db3f077449b05e4bea09ae
Dert1129 <nathancreger1111@gmail.com>
2023-08-01 10:59:47 -0400

    fix query

--- data_management/watcher/dlu_package_inventory.py
+++ data_management/watcher/dlu_package_inventory.py
@@ -20,12 +20,12 @@ class DLUPackageInventory:
         
     def get_dlu_file(self, status):
         return self.db.get_data(
-            "SELECT * FROM data_management.dlu_package_inventory WHERE ready_to_move_from_globus == %s AND dlu_package_id AND globus_dlu_status == null",
+            "SELECT * FROM data_management.dlu_package_inventory WHERE ready_to_move_from_globus = %s AND dlu_package_id AND globus_dlu_status = null",
             (status),
         )
     
     def set_dlu_file_waiting(self, status, package_id):
         return self.db.insert_data(
-            "UPDATE data_management.dlu_package_inventory WHERE ready_to_move_from_globus_ == %s AND dlu_package_id == %s AND globus_dlu_status == waiting",
+            "UPDATE data_management.dlu_package_inventory WHERE ready_to_move_from_globus = %s AND dlu_package_id == %s AND globus_dlu_status = waiting",
             (status, package_id),
         )
