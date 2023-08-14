d8455c3a039045d66615f3c8cd3815b3ba356cec
HaneenT <haneen@umich.edu>
2023-08-14 13:45:41 -0400

    KPMP-4658: added route to return ready packages

--- data_management/app.py
+++ data_management/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from services.data_management import DataManagement
+from services.dlu_package_inventory import DLUPackageInventory
 from services.dlu_utils import dlu_package_dict_to_tuple, dlu_file_dict_to_tuple
 
 app = Flask(__name__)
@@ -16,6 +17,12 @@ def add_dlu_package():
     data_management.insert_dlu_package(content_tuple)
     return content_tuple[0]
 
+@app.route("/v1/dlu/package/ready", methods=["GET"])
+def get_ready_packages():
+    dlu_package_inventory = DLUPackageInventory()
+    dlu_package_inventory.reconnect()
+    files = dlu_package_inventory.get_ready_packages()
+    return files
 
 @app.route("/v1/dlu/package/<package_id>", methods=["POST"])
 def update_dlu_package(package_id):
