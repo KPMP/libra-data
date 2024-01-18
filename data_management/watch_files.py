853ff8316c09e7e232cdf5445818d0ace6bfb7f8
HaneenT <haneen@umich.edu>
2024-01-18 09:14:56 -0500

    KPMP-4656: updated file mover

--- data_management/watch_files.py
+++ data_management/watch_files.py
@@ -54,31 +54,32 @@ class DLUWatcher:
         return dlu_files
 
     def move_packages_to_DLU(self, packages):
-        for _, package in enumerate(packages):
-            package_id = package['dlu_package_id']
-            logger.info("Moving package " + package_id)
+        processing_count = self.db.get_processing_packages()
+        if len(processing_count) < 2:
+            for _, package in enumerate(packages):
+                package_id = package['dlu_package_id']
+                logger.info("Moving package " + package_id)
 
-            self.data_management.update_dlu_package(package_id, { "globus_dlu_status": "processing" })
+                self.data_management.update_dlu_package(package_id, { "globus_dlu_status": "processing" })
+                globus_data_directory = os.environ['globus_data_directory'] + '/' + package_id
+                if not os.path.isdir(globus_data_directory):
+                    error_msg = "Error: package " + package_id + " not found in directory " + os.environ['globus_data_directory'] + "."
+                    logger.info(error_msg + " Skipping.")
+                    self.data_management.update_dlu_package(package_id, { "globus_dlu_status": error_msg })
+                    continue
+    
+                directory_info = DirectoryInfo(globus_data_directory)
+                directory_info.get_directory_information()
 
-            globus_data_directory = os.environ['globus_data_directory'] + '/' + package_id
-            if not os.path.isdir(globus_data_directory):
-                error_msg = "Error: package " + package_id + " not found in directory " + os.environ['globus_data_directory'] + "."
-                logger.info(error_msg + " Skipping.")
-                self.data_management.update_dlu_package(package_id, { "globus_dlu_status": error_msg })
-                continue
+                if directory_info.file_count == 0:
+                    error_msg = "Error: package " + package_id + " has no files."
+                    logger.info(error_msg + " Skipping.")
+                    self.data_management.update_dlu_package(package_id, { "globus_dlu_status": error_msg })
+                    continue
 
-            directory_info = DirectoryInfo(globus_data_directory)
-            directory_info.get_directory_information()
-
-            if directory_info.file_count == 0:
-                error_msg = "Error: package " + package_id + " has no files."
-                logger.info(error_msg + " Skipping.")
-                self.data_management.update_dlu_package(package_id, { "globus_dlu_status": error_msg })
-                continue
-
-            self.dlu_file_handler.copy_files(package_id, self.process_file_paths(directory_info.file_details))
-            self.data_management.update_dlu_package(package_id, { "globus_dlu_status": "success" })
-            self.dlu_state.set_package_state(package_id, PackageState.UPLOAD_SUCCEEDED)
+                self.dlu_file_handler.copy_files(package_id, self.process_file_paths(directory_info.file_details))
+                self.data_management.update_dlu_package(package_id, { "globus_dlu_status": "success" })
+                self.dlu_state.set_package_state(package_id, PackageState.UPLOAD_SUCCEEDED)
 
 
 
