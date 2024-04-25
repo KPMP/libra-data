dd17b2f5cd0fd2ded57c496e556cf5612f80d907
rlreamy <34109594+rlreamy@users.noreply.github.com>
2024-04-25 10:37:05 -0400

    Update dlu_filesystem.py

--- data_management/services/dlu_filesystem.py
+++ data_management/services/dlu_filesystem.py
@@ -1,10 +1,11 @@
 import os
 import logging
 import shutil
-import hashlib
+from hashlib import md5
 import uuid
 from zarr_checksum import compute_zarr_checksum
 from zarr_checksum.generators import yield_files_local
+from mmap import mmap, ACCESS_READ
 
 logger = logging.getLogger("DLUFilesystem")
 logger.setLevel(logging.INFO)
@@ -25,11 +26,8 @@ def calculate_checksum(file_path: str):
     if os.path.isdir(file_path):
         return "0"
     if ".zarr" not in file_path:
-        with open(file_path, "rb") as f:
-            file_hash = hashlib.md5()
-            while chunk := f.read(4096):
-                file_hash.update(chunk)
-        return file_hash.hexdigest()
+        with open(file_path) as f, mmap(f.fileno(), 0, access=ACCESS_READ) as f:
+            return md5(f).hexdigest();
     else:
         return compute_zarr_checksum(yield_files_local(file_path)).md5
 
