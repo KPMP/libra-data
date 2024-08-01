de4ad6fff8889f6f9370a7166014b91051f2b72e
dert1129 <nathancreger1111@gmail.com>
2024-08-01 13:07:35 -0400

    change run.sh to use new image tag

--- data_management/run.sh
+++ data_management/run.sh
@@ -1,1 +1,1 @@
-docker run --log-driver=none -a stdin -a stdout -a stderr --network dataLake --env-file .env kingstonduo/data-management:latest python ./main.py $1 $2 $3 $4
+docker run --log-driver=none -a stdin -a stdout -a stderr --network dataLake --env-file .env kingstonduo/data-management:KPMP-5472_remove-spectrack-connection python ./main.py $1 $2 $3 $4
