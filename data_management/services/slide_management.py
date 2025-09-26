1f58f5892c726fffc5d9dc54ab71b5e89ffb53f7
Becky Reamy <rlreamy@umich.edu>
2025-09-26 13:29:47 -0400

    KPMP-6195: handle issues with file_location and file name determination

--- data_management/services/slide_management.py
+++ data_management/services/slide_management.py
@@ -43,6 +43,7 @@ def determine_stain(stain_info, block_id):
         return "SIL"
     return None
 
+
 def calculate_denominator(slides_for_kit, block_id):
     denominator = 0
     for slide in slides_for_kit:
@@ -74,24 +75,37 @@ class SlideManagement:
     def process_slide_manifest_imports(self):
         new_records = self.db.get_new_slide_manifest_import_rows()
         for record in new_records:
+            record_in_error = False
+            error_message = ""
             kit_id = record["outside_acc"]
             image_id = record["image_id"]
             redcap_id = self.db.get_spectrack_redcap_record_id(kit_id)
-            new_file_name = self.determine_new_slide_name(sample_id=record["barcode_id"], kit_id=kit_id,
-                                                          stain_info=record["stain"], block_id=record["block_id"])
-            file_location = PureWindowsPath(record['file_location'])
-            source_file_name = file_location.name
-            source_folder_name = file_location.parent.name
-            
+            if record["barcode_id"] is not None:
+                new_file_name = self.determine_new_slide_name(sample_id=record["barcode_id"], kit_id=kit_id,
+                                                              stain_info=record["stain"], block_id=record["block_id"])
+            else:
+                new_file_name = None
+                record_in_error = True
+                error_message = "Missing sample id, unable to determine file name."
+
+            # Sometimes the file location gets copied in with a single leading slash
+            if record["file_location"].count("\\") < 2:
+                source_file_name = None
+                source_folder_name = None
+                record_in_error = True
+                error_message = error_message + "Unable to determine source file or folder."
+            else:
+                file_location = PureWindowsPath(record['file_location'])
+                source_file_name = file_location.name
+                source_folder_name = file_location.parent.name
+
             slide_scan = SlideScanModel(image_id=image_id, redcap_id=redcap_id, kit_id=kit_id,
                                         new_file_name=new_file_name, source_file_name=source_file_name,
                                         source_folder_name=source_folder_name)
-            self.db.insert_into_slide_scan_curation(slide_scan.get_dmd_tuple())
 
-            # If we were unable to determine the filename, we want to update this with an error message, after a
-            # record is in slide_scan_curation
-            if new_file_name is None:
-                self.db.set_error_message_slide_scan_curation("Unknown stain type", image_id=image_id)
+            self.db.insert_into_slide_scan_curation(slide_scan.get_dmd_tuple())
+            if record_in_error:
+                self.db.set_error_message_slide_scan_curation(image_id=image_id, error=error_message)
 
     def determine_new_slide_name(self, sample_id: str, kit_id: str, stain_info: str, block_id: str):
         slides_for_kit = self.db.get_slide_manifest_import_by_kit(kit_id, stain_info)
